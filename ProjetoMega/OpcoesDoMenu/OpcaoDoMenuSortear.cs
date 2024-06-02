324612cc386591e7dce1e103c3f77d31eb8e994a
Desert0rluz <luizmartinsluz@gmail.com>
2024-06-02 11:48:03 -0300

    0.1

---
+++ ProjetoMega/OpcoesDoMenu/OpcaoDoMenuSortear.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjetoMega.OpcoesDoMenu;
+
+internal class OpcaoDoMenuSortear
+{
+    public static void OpcaoDoMenu()
+    {
+        List<int> numerosSorteados = SortearNumeros();
+        Console.WriteLine("Números sorteados:");
+        foreach (int numero in numerosSorteados)
+        {
+            Console.WriteLine(numero);
+        } 
+    }
+    static List<int> SortearNumeros()
+    {
+        Random random = new Random();
+        HashSet<int> numerosSorteados = new HashSet<int>();
+
+        while (numerosSorteados.Count < 6)
+        {
+            int numero = random.Next(1, 61);
+            numerosSorteados.Add(numero);
+        }
+        return new List<int>(numerosSorteados);
+    }
+}
+
---
+++ ProjetoMega/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoMega.OpcoesDoMenu;
+
+Console.WriteLine();
+Console.WriteLine(@"
+███╗░░░███╗███████╗░██████╗░░█████╗░░██████╗███████╗███╗░░██╗░█████╗░
+████╗░████║██╔════╝██╔════╝░██╔══██╗██╔════╝██╔════╝████╗░██║██╔══██╗
+██╔████╔██║█████╗░░██║░░██╗░███████║╚█████╗░█████╗░░██╔██╗██║███████║
+██║╚██╔╝██║██╔══╝░░██║░░╚██╗██╔══██║░╚═══██╗██╔══╝░░██║╚████║██╔══██║
+██║░╚═╝░██║███████╗╚██████╔╝██║░░██║██████╔╝███████╗██║░╚███║██║░░██║
+╚═╝░░░░░╚═╝╚══════╝░╚═════╝░╚═╝░░╚═╝╚═════╝░╚══════╝╚═╝░░╚══╝╚═╝░░╚═╝");
+Console.WriteLine();
+Console.WriteLine("Bem vindo ao projeto, aqui sua sorte é nossa aposta.\n");
+Console.WriteLine("Digite uma das opções do menu abaixo: \n");
+Console.WriteLine("1 - Sortear numeros");
+Console.WriteLine("2 - Conferir resultados");
+Console.WriteLine("3 - Arquivar jogo");
+Console.WriteLine("4 - Sair\n");
+Console.Write("Sua opção: ");
+
+string opcaoSelecionada = Console.ReadLine()!;
+int opcaoMenu = int.Parse(opcaoSelecionada);
+
+switch (opcaoMenu)
+{
+    case 1: OpcaoDoMenuSortear.OpcaoDoMenu();
+        break;
+      
+}
+
+
